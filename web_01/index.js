29d48dacea3f9401da6b9b0cde99c47c3023e971
linnefromice <haruyama.arata@moneyforward.co.jp>
2020-11-06 02:48:35 +0900

    add selecting kind of voice

--- web_01/index.js
+++ web_01/index.js
@@ -1,8 +1,28 @@
 (function() {
   // CC0 http://creativecommons.org/publicdomain/zero/1.0/
+  if (window.speechSynthesis) {
+    let voices = [];
+    function setVoices() {
+      if (voices.length) return;
+      voices = speechSynthesis.getVoices();
+      if (!voices.length) return;
+      voices
+        .filter(v => v.lang.startsWith("ja"))
+        .forEach(v => {
+          let opt = document.createElement("option");
+          opt.text = v.name;
+          opt.voice = v;
+          document.getElementById("voice").appendChild(opt);
+        });
+    }
+    speechSynthesis.addEventListener("voiceschanged", setVoices);
+    setVoices();
+  }
   document.getElementById("button1").addEventListener("click", () => {
-    if (!window.speechSynthesis) return;
+    let opt = document.getElementById("voice").selectedOptions;
+    if (!opt.length) return;
     let u = new SpeechSynthesisUtterance(textarea.value);
+    u.voice = opt[0].voice;
     u.lang = "ja";
     speechSynthesis.speak(u);
   });
